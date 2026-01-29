f5cd03d6eac90c03c05f0e03d40be9952eb60542
추지인 <cnchoo25@naver.com>
2026-01-30 00:45:37 +0900

    feat: 장바구니 데스크탑, 모바일 웹소켓 연결

---
+++ SHOPPY-BE/src/main/java/ssafy/rtc/shoppy/shopping/dto/ShoppingItemDeleteRequestDto.java
@@ -0,0 +1,12 @@
+package ssafy.rtc.shoppy.shopping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShoppingItemDeleteRequestDto {
+    private Long shoppingItemId;
+}
