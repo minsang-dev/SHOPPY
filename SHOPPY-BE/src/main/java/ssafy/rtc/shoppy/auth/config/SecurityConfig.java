cebbcc734a2330ebada99031e67003a935ebdfec
김민우 <rlamw2000@naver.com>
2026-01-29 09:39:15 +0900

    feat:websocket permit 권한 추가

--- SHOPPY-BE/src/main/java/ssafy/rtc/shoppy/auth/config/SecurityConfig.java
+++ SHOPPY-BE/src/main/java/ssafy/rtc/shoppy/auth/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
+                                "/ws/**",
                                 "/rooms/**",
                                 "/webrtc/**",
                                 "/shopping/**",
