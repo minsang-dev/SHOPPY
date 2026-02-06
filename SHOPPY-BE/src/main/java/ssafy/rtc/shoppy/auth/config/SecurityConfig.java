35bcc328415d5cbd7fa8592885a8c6ff2ace5765
김민상 <minsang1233@gmail.com>
2026-02-06 22:11:01 +0900

    feat: 모니터링 시스템 구축

--- SHOPPY-BE/src/main/java/ssafy/rtc/shoppy/auth/config/SecurityConfig.java
+++ SHOPPY-BE/src/main/java/ssafy/rtc/shoppy/auth/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
                         .requestMatchers("/ws/**", "/api/ws/**").permitAll()
                         .requestMatchers("/api/actuator/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
 
                         // Room: 공개 엔드포인트
                         .requestMatchers(HttpMethod.GET, "/rooms/code/**").permitAll()
